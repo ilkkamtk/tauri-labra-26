839a93c7cd264b7180ad48aec965ecc456b7fd6d
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-01-26 09:38:23 +0200

    Refactor code structure for improved readability and maintainability

--- src/views/DetectGesture.tsx
+++ src/views/DetectGesture.tsx
@@ -1,5 +1,25 @@
+import Camera from '@/components/Camera';
+import { ThumbsUpIcon } from 'lucide-react';
+import { useRef } from 'react';
+
 const DetectGesture = () => {
-  return <div>DetectGesture</div>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  return (
+    <>
+      <section className="w-full">
+        <Camera ref={videoRef} width={800} height={480} />
+      </section>
+      <section className="w-full">
+        <div className="absolute top-24 left-0 p-4 bg-stone-900">
+          <p>
+            faceName: 'jotain' &nbsp; <ThumbsUpIcon />
+          </p>
+          <p>Ohjeet</p>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default DetectGesture;
